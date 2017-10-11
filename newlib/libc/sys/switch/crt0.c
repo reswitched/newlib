3cdb66b83cd38bc4df81bddf115f3d3b74a62497
misson20000 <xenotoad@xenotoad.net>
2017-10-11 16:12:26 -0700

    ???

--- newlib/libc/sys/switch/crt0.c
+++ newlib/libc/sys/switch/crt0.c
@@ -1,9 +1,10 @@
 #include <fcntl.h>
- 
+
+extern char **environ;
 extern void exit(int code);
-extern int main ();
+extern int main(int argc, char **argv);
  
 void _start() {
-    int ex = main();
-    exit(ex);
+  int ex = main(0, 0);
+  exit(ex);
 }
