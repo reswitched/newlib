52583e13fed03c9282d48c3ff799d23e7eb6b715
misson20000 <xenotoad@xenotoad.net>
2017-10-10 20:46:53 -0700

    remove clang stuff from configure script (use environment variables instead) and implement stubs for switch syscalls

--- newlib/libc/sys/switch/syscalls.c
+++ newlib/libc/sys/switch/syscalls.c
@@ -7,23 +7,78 @@
 #include <sys/time.h>
 #include <stdio.h>
  
-void _exit();
-int close(int file);
-char **environ; /* pointer to array of char * strings that define the current environment variables */
-int execve(char *name, char **argv, char **env);
-int fork();
-int fstat(int file, struct stat *st);
-int getpid();
-int isatty(int file);
-int kill(int pid, int sig);
-int link(char *old, char *new);
-int lseek(int file, int ptr, int dir);
-int open(const char *name, int flags, ...);
-int read(int file, char *ptr, int len);
-caddr_t sbrk(int incr);
-int stat(const char *file, struct stat *st);
-clock_t times(struct tms *buf);
-int unlink(char *name);
-int wait(int *status);
-int write(int file, char *ptr, int len);
-int gettimeofday(struct timeval *__restrict p, void *__restrict z);
+void _exit(); // implemented in libtransistor crt0
+
+int _close(int file) {
+  return -1;
+}
+
+char *_environ[] = {NULL};
+
+int _execve(char *name, char **argv, char **env) {
+  return -1;
+}
+
+int _fork() {
+  return -1;
+}
+
+int _fstat(int file, struct stat *st) {
+  return -1;
+}
+
+int _getpid() {
+  return -1;
+}
+
+int _isatty(int file) {
+  return -1;
+}
+
+int _kill(int pid, int sig) {
+  return -1;
+}
+
+int _link(char *old, char *new) {
+  return -1;
+}
+
+int _lseek(int file, int ptr, int dir) {
+  return -1;
+}
+
+int _open(const char *name, int flags, ...) {
+  return -1;
+}
+
+int _read(int file, char *ptr, int len) {
+  return -1;
+}
+
+caddr_t _sbrk(int incr) {
+  return (void*) -1;
+}
+
+int _stat(const char *file, struct stat *st) {
+  return -1;
+}
+
+clock_t _times(struct tms *buf) {
+  return (clock_t) -1;
+}
+
+int _unlink(char *name) {
+  return -1;
+}
+
+int _wait(int *status) {
+  return -1;
+}
+
+int _write(int file, char *ptr, int len) {
+  return -1;
+}
+
+int _gettimeofday(struct timeval *__restrict p, void *__restrict z) {
+  return -1;
+}
