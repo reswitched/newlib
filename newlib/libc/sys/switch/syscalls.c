42f2bcf2b36e494c6747f28d1006088482af4a59
misson20000 <xenotoad@xenotoad.net>
2017-10-11 22:08:52 -0700

    reentrant syscalls

--- newlib/libc/sys/switch/syscalls.c
+++ newlib/libc/sys/switch/syscalls.c
@@ -11,85 +11,102 @@
 
 void _exit(); // implemented in libtransistor crt0
 
-int _close(int file) {
+int _close_r(struct _reent *reent, int file) {
+  reent->_errno = ENOSYS;
   return -1;
 }
 
 char *_environ[] = {NULL};
 
-int _execve(char *name, char **argv, char **env) {
+int _execve_r(struct _reent *reent, char *name, char **argv, char **env) {
+  reent->_errno = ENOSYS;
   return -1;
 }
 
-int _fork() {
+int _fork_r(struct _reent *reent) {
+  reent->_errno = ENOSYS;
   return -1;
 }
 
-int _fstat(int file, struct stat *st) {
+int _fstat_r(struct _reent *reent, int file, struct stat *st) {
+  reent->_errno = ENOSYS;
   return -1;
 }
 
-int _getpid() {
+int _getpid_r(struct _reent *reent) {
+  reent->_errno = ENOSYS;
   return -1;
 }
 
-int _isatty(int file) {
+int _isatty_r(struct _reent *reent, int file) {
+  reent->_errno = ENOSYS;
   return -1;
 }
 
-int _kill(int pid, int sig) {
+int _kill_r(struct _reent *reent, int pid, int sig) {
+  reent->_errno = ENOSYS;
   return -1;
 }
 
-int _link(char *old, char *new) {
+int _link_r(struct _reent *reent, char *old, char *new) {
+  reent->_errno = ENOSYS;
   return -1;
 }
 
-int _lseek(int file, int ptr, int dir) {
+int _lseek_r(struct _reent *reent, int file, int ptr, int dir) {
+  reent->_errno = ENOSYS;
   return -1;
 }
 
-int _open(const char *name, int flags, ...) {
+int _open_r(struct _reent *reent, const char *name, int flags, ...) {
+  reent->_errno = ENOSYS;
   return -1;
 }
 
-int _read(int file, char *ptr, int len) {
+int _read_r(struct _reent *reent, int file, char *ptr, int len) {
+  reent->_errno = ENOSYS;
   return -1;
 }
 
 static char bss_heap[BSS_HEAP_SIZE];
 static int bss_heap_allocated = 0;
 
-caddr_t _sbrk(int incr) {
+caddr_t _sbrk_r(struct _reent *reent, int incr) {
   bss_heap_allocated+= incr;
   if(bss_heap_allocated > sizeof(bss_heap)) {
-    errno = ENOMEM;
+    reent->_errno = ENOMEM;
     return (void*) -1;
   }
 
   return bss_heap;
 }
 
-int _stat(const char *file, struct stat *st) {
+int _stat_r(struct _reent *reent, const char *file, struct stat *st) {
+  reent->_errno = ENOSYS;
   return -1;
 }
 
-clock_t _times(struct tms *buf) {
+clock_t _times_r(struct _reent *reent, struct tms *buf) {
+  reent->_errno = ENOSYS;
   return (clock_t) -1;
 }
 
-int _unlink(char *name) {
+int _unlink_r(struct _reent *reent, char *name) {
+  reent->_errno = ENOSYS;
   return -1;
 }
 
-int _wait(int *status) {
+int _wait_r(struct _reent *reent, int *status) {
+  reent->_errno = ENOSYS;
   return -1;
 }
 
-int _write(int file, char *ptr, int len) {
+int _write_r(struct _reent *reent, int file, char *ptr, int len) {
+  reent->_errno = ENOSYS;
   return -1;
 }
 
-int _gettimeofday(struct timeval *__restrict p, void *__restrict z) {
+int _gettimeofday_r(struct _reent *reent, struct timeval *__restrict p, void *__restrict z) {
+  reent->_errno = ENOSYS;
   return -1;
 }
