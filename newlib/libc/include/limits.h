7fb01db6fa8d84e30aaf822e5cf788ee45f54f4c
David Freitag <dvdfreitag@gmail.com>
2017-11-27 01:44:10 -0500

    Add missing headers
    nostdinc is enabled for libtransistor, missing header files are added here. They have all been copied from clang: https://clang.llvm.org/doxygen/dir_32af269ab941e393bd1c05d50cd12728.html. This will in all likelihood end in missing system headers in the future, but we should be able to source them from clang as with these headers.

--- newlib/libc/include/limits.h
+++ newlib/libc/include/limits.h
@@ -1,146 +1,112 @@
-#ifndef _LIBC_LIMITS_H_
-# define _LIBC_LIMITS_H_	1
+/*===---- limits.h - Standard header for integer sizes --------------------===*\
+ *
+ * Copyright (c) 2009 Chris Lattner
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+\*===----------------------------------------------------------------------===*/
 
-#include <newlib.h>
-#include <sys/cdefs.h>
+#ifndef __CLANG_LIMITS_H
+#define __CLANG_LIMITS_H
 
-# ifdef _MB_LEN_MAX
-#  define MB_LEN_MAX	_MB_LEN_MAX
-# else
-#  define MB_LEN_MAX    1
-# endif
-
-/* Maximum number of positional arguments, if _WANT_IO_POS_ARGS.  */
-# ifndef NL_ARGMAX
-#  define NL_ARGMAX 32
-# endif
-
-/* if do not have #include_next support, then we
-   have to define the limits here. */
-# if !defined __GNUC__ || __GNUC__ < 2
-
-#  ifndef _LIMITS_H
-#   define _LIMITS_H	1
-
-#   include <sys/config.h>
-
-/* Number of bits in a `char'.  */
-#   undef CHAR_BIT
-#   define CHAR_BIT 8
-
-/* Minimum and maximum values a `signed char' can hold.  */
-#   undef SCHAR_MIN
-#   define SCHAR_MIN (-128)
-#   undef SCHAR_MAX
-#   define SCHAR_MAX 127
-
-/* Maximum value an `unsigned char' can hold.  (Minimum is 0).  */
-#   undef UCHAR_MAX
-#   define UCHAR_MAX 255
-
-/* Minimum and maximum values a `char' can hold.  */
-#   ifdef __CHAR_UNSIGNED__
-#    undef CHAR_MIN
-#    define CHAR_MIN 0
-#    undef CHAR_MAX
-#    define CHAR_MAX 255
-#   else
-#    undef CHAR_MIN
-#    define CHAR_MIN (-128)
-#    undef CHAR_MAX
-#    define CHAR_MAX 127
-#   endif
-
-/* Minimum and maximum values a `signed short int' can hold.  */
-#   undef SHRT_MIN
-/* For the sake of 16 bit hosts, we may not use -32768 */
-#   define SHRT_MIN (-32767-1)
-#   undef SHRT_MAX
-#   define SHRT_MAX 32767
-
-/* Maximum value an `unsigned short int' can hold.  (Minimum is 0).  */
-#   undef USHRT_MAX
-#   define USHRT_MAX 65535
-
-/* Minimum and maximum values a `signed int' can hold.  */
-#   ifndef __INT_MAX__
-#    define __INT_MAX__ 2147483647
-#   endif
-#   undef INT_MIN
-#   define INT_MIN (-INT_MAX-1)
-#   undef INT_MAX
-#   define INT_MAX __INT_MAX__
-
-/* Maximum value an `unsigned int' can hold.  (Minimum is 0).  */
-#   undef UINT_MAX
-#   define UINT_MAX (INT_MAX * 2U + 1)
-
-/* Minimum and maximum values a `signed long int' can hold.
-   (Same as `int').  */
-#   ifndef __LONG_MAX__
-#    if defined (__alpha__) || (defined (__sparc__) && defined(__arch64__)) || defined (__sparcv9)
-#     define __LONG_MAX__ 9223372036854775807L
-#    else
-#     define __LONG_MAX__ 2147483647L
-#    endif /* __alpha__ || sparc64 */
-#   endif
-#   undef LONG_MIN
-#   define LONG_MIN (-LONG_MAX-1)
-#   undef LONG_MAX
-#   define LONG_MAX __LONG_MAX__
-
-/* Maximum value an `unsigned long int' can hold.  (Minimum is 0).  */
-#   undef ULONG_MAX
-#   define ULONG_MAX (LONG_MAX * 2UL + 1)
-
-#   ifndef __LONG_LONG_MAX__
-#    define __LONG_LONG_MAX__ 9223372036854775807LL
-#   endif
-
-#   if __ISO_C_VISIBLE >= 1999
-/* Minimum and maximum values a `signed long long int' can hold.  */
-#    undef LLONG_MIN
-#    define LLONG_MIN (-LLONG_MAX-1)
-#    undef LLONG_MAX
-#    define LLONG_MAX __LONG_LONG_MAX__
-
-/* Maximum value an `unsigned long long int' can hold.  (Minimum is 0).  */
-#    undef ULLONG_MAX
-#    define ULLONG_MAX (LLONG_MAX * 2ULL + 1)
-#   endif
-
-#  if __GNU_VISIBLE
-/* Minimum and maximum values a `signed long long int' can hold.  */
-#    undef LONG_LONG_MIN
-#    define LONG_LONG_MIN (-LONG_LONG_MAX-1)
-#    undef LONG_LONG_MAX
-#    define LONG_LONG_MAX __LONG_LONG_MAX__
-
-/* Maximum value an `unsigned long long int' can hold.  (Minimum is 0).  */
-#    undef ULONG_LONG_MAX
-#    define ULONG_LONG_MAX (LONG_LONG_MAX * 2ULL + 1)
-#   endif
-
-#  endif /* _LIMITS_H  */
-# endif	 /* GCC 2.  */
-
-#endif	 /* !_LIBC_LIMITS_H_ */
-
+/* The system's limits.h may, in turn, try to #include_next GCC's limits.h.
+   Avert this #include_next madness. */
 #if defined __GNUC__ && !defined _GCC_LIMITS_H_
-/* `_GCC_LIMITS_H_' is what GCC's file defines.  */
-# include_next <limits.h>
-#endif /* __GNUC__ && !_GCC_LIMITS_H_ */
-
-#ifndef _POSIX2_RE_DUP_MAX
-/* The maximum number of repeated occurrences of a regular expression
- *    permitted when using the interval notation `\{M,N\}'.  */
-#define _POSIX2_RE_DUP_MAX              255
-#endif /* _POSIX2_RE_DUP_MAX  */
-
-#ifndef ARG_MAX
-#define ARG_MAX		4096
+#define _GCC_LIMITS_H_
 #endif
 
-#ifndef PATH_MAX
-#define PATH_MAX	4096
+/* Many system headers try to "help us out" by defining these.  No really, we
+   know how big each datatype is. */
+#undef  SCHAR_MIN
+#undef  SCHAR_MAX
+#undef  UCHAR_MAX
+#undef  SHRT_MIN
+#undef  SHRT_MAX
+#undef  USHRT_MAX
+#undef  INT_MIN
+#undef  INT_MAX
+#undef  UINT_MAX
+#undef  LONG_MIN
+#undef  LONG_MAX
+#undef  ULONG_MAX
+
+#undef  CHAR_BIT
+#undef  CHAR_MIN
+#undef  CHAR_MAX
+
+/* C90/99 5.2.4.2.1 */
+#define SCHAR_MAX __SCHAR_MAX__
+#define SHRT_MAX  __SHRT_MAX__
+#define INT_MAX   __INT_MAX__
+#define LONG_MAX  __LONG_MAX__
+
+#define SCHAR_MIN (-__SCHAR_MAX__-1)
+#define SHRT_MIN  (-__SHRT_MAX__ -1)
+#define INT_MIN   (-__INT_MAX__  -1)
+#define LONG_MIN  (-__LONG_MAX__ -1L)
+
+#define UCHAR_MAX (__SCHAR_MAX__*2  +1)
+#define USHRT_MAX (__SHRT_MAX__ *2  +1)
+#define UINT_MAX  (__INT_MAX__  *2U +1U)
+#define ULONG_MAX (__LONG_MAX__ *2UL+1UL)
+
+#ifndef MB_LEN_MAX
+#define MB_LEN_MAX 1
 #endif
+
+#define CHAR_BIT  __CHAR_BIT__
+
+#ifdef __CHAR_UNSIGNED__  /* -funsigned-char */
+#define CHAR_MIN 0
+#define CHAR_MAX UCHAR_MAX
+#else
+#define CHAR_MIN SCHAR_MIN
+#define CHAR_MAX __SCHAR_MAX__
+#endif
+
+/* C99 5.2.4.2.1: Added long long.
+   C++11 18.3.3.2: same contents as the Standard C Library header <limits.h>.
+ */
+#if __STDC_VERSION__ >= 199901L || __cplusplus >= 201103L
+
+#undef  LLONG_MIN
+#undef  LLONG_MAX
+#undef  ULLONG_MAX
+
+#define LLONG_MAX  __LONG_LONG_MAX__
+#define LLONG_MIN  (-__LONG_LONG_MAX__-1LL)
+#define ULLONG_MAX (__LONG_LONG_MAX__*2ULL+1ULL)
+#endif
+
+/* LONG_LONG_MIN/LONG_LONG_MAX/ULONG_LONG_MAX are a GNU extension.  It's too bad
+   that we don't have something like #pragma poison that could be used to
+   deprecate a macro - the code should just use LLONG_MAX and friends.
+ */
+#if defined(__GNU_LIBRARY__) ? defined(__USE_GNU) : !defined(__STRICT_ANSI__)
+
+#undef   LONG_LONG_MIN
+#undef   LONG_LONG_MAX
+#undef   ULONG_LONG_MAX
+
+#define LONG_LONG_MAX  __LONG_LONG_MAX__
+#define LONG_LONG_MIN  (-__LONG_LONG_MAX__-1LL)
+#define ULONG_LONG_MAX (__LONG_LONG_MAX__*2ULL+1ULL)
+#endif
+
+#endif /* __CLANG_LIMITS_H */
