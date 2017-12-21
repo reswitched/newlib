8b65c1f201b90d19e7fd674f2ad110c95cb0a342
misson20000 <xenotoad@xenotoad.net>
2017-12-21 13:03:03 -0800

    remove syscalls (to be moved to libtransistor)

--- newlib/libc/sys/switch/crt0.c
+++ newlib/libc/sys/switch/crt0.c
@@ -1,10 +1,1 @@
-#include <fcntl.h>
-
-extern char **environ;
-extern void exit(int code);
-extern int main(int argc, char **argv);
- 
-void _start() {
-  int ex = main(0, 0);
-  exit(ex);
-}
+/* empty */
--- newlib/libc/sys/switch/sched.c
+++
@@ -1,5 +0,0 @@
-#include <libtransistor/nx.h>
-
-int sched_yield() {
-	return svcSleepThread(0) != 0 ? -1 : 0;
-}
